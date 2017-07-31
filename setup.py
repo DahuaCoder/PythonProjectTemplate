df2de2739f5928b35c803646f338af95b59af425
Dario Steiner <steiner.stdio@gmail.com>
2017-07-31 21:31:02 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+
+from distutils.core import setup
+
+setup(
+    name='PythonProjectTemplate',
+    version='0.1dev',
+    packages=['projecttemplate', ],
+    license='Dario Steiner license',
+    long_description=open('README.txt').read(),
+)
